201d35a14f644d45f94d4540dd1108966daa0be5
Jakub Nakielski <jakubnakielski2@gmail.com>
2020-01-02 20:18:53 +0100

    add compareAnswer function

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,30 @@
 const btn = document.querySelector(".startBtn");
-const numberOfGames = document.querySelector(".rightContainer__numbers");
-const wins = document.querySelector(".rightContainer__wins");
-const looses = document.querySelector(".rightContainer__losses");
-const draws = document.querySelector(".rightContainer__draws");
+const numberOfGames = document.querySelector(".rightContainer__numbers > span");
+const wins = document.querySelector(".rightContainer__wins > span");
+const looses = document.querySelector(".rightContainer__losses > span");
+const draws = document.querySelector(".rightContainer__draws > span");
+let yourChoice = "";
+let aiChoice = "";
+let winner = "";
 
 const choiceArray = [...document.querySelectorAll(".choiceContainer__item")];
 const gameSummary = {
-  numbers: "0",
-  wins: "0",
-  losses: "0",
-  draws: "0"
+  numbers: 0,
+  wins: 0,
+  losses: 0,
+  draws: 0
 };
 const game = {
   playerSelection: "",
   aiSelection: ""
 };
 const writeResults = () => {
-    resultsHeader = 
+  numberOfGames.textContent = gameSummary.numbers;
+  wins.textContent = gameSummary.wins;
+  looses.textContent = gameSummary.losses;
+  draws.textContent = gameSummary.draws;
 };
+
 const compareAnswers = () => {
   const playerAnswer = game.playerSelection;
   const aiAnswer = game.aiSelection;
@@ -46,7 +53,6 @@ const getRandomAnswer = () => {
 
 const selectAnswer = function() {
   game.playerSelection = this.dataset.option;
-
   choiceArray.forEach(e => {
     e.style.borderColor = "";
   });
@@ -58,9 +64,12 @@ const playGame = () => {
     el.addEventListener("click", selectAnswer);
   });
 
+  gameSummary.numbers++;
   getRandomAnswer();
   compareAnswers();
-  writeResults();
-};
 
-btn.addEventListener("click", playGame);
+  writeResults();
+
+  btn.addEventListener("click", playGame);
+};
+playGame();
